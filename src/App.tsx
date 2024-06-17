ab2df8508a80009dd54a9adbca3c7febfee96b77
G2p1 <andrii.tiuitunnyk@gmail.com>
2024-06-17 05:08:24 +0300

    xd

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import './App.css';
+import heart from './img/heart.png';
+
+
+function App() {
+
+function soundPlay(){
+
+  new Audio(`/s.mp3`).play();
+}
+
+  return (
+    <>
+    <img className='heart1' src={heart}></img>
+      <button className='textviki' onClick={soundPlay}>Вікторія хоче какать!!!!</button>
+      
+    </>
+  )
+}
+
+export default App
